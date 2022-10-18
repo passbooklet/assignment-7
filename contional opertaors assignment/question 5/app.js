c7611de99e157a86584287f66fb67222ca96e8e8
Adan <109463743+passbooklet@users.noreply.github.com>
2022-10-18 22:34:29 +0500

    Add files via upload

---
+++ contional opertaors assignment/question 5/app.js
@@ -0,0 +1,4 @@
+//Write a ts program to check whether character is an alphabet or not using conditional operator.
+var a = prompt("please enter your  value");
+var d1 = (a == "A" || a == "B" || a == "C" || a == "D" || a == "E" || a == "F" || a == "G" || a == "H" || a == "I" || a == "J" || a == "K" || a == "L" || a == "M" || a == "N" || a == "O" || a == "P" || a == "Q" || a == "R" || a == "S" || a == "T" || a == "U" || a == "V" || a == "W" || a == "X" || a == "Y" || a == "Z" || a == "a" || a == "b" || a == "c" || a == "d" || a == "e" || a == "f" || a == "g" || a == "h" || a == "i" || a == "j" || a == "k" || a == "l" || a == "m" || a == "n" || a == "o" || a == "p" || a == "q" || a == "r" || a == "s" || a == "t" || a == "u" || a == "v" || a == "w" || a == "x" || a == "y" || a == "z" ? "it is alphabet" : "it is not an alphabet");
+console.log(d1);
